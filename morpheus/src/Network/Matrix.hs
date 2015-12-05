6d96ce2123f1dc1f9cc236b5a8ff61332a3068e5
Christine Dodrill <xena@yolo-swag.com>
2015-12-05 13:14:37 -0800

    pass tests

--- morpheus/src/Network/Matrix.hs
+++ morpheus/src/Network/Matrix.hs
@@ -1,8 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.Matrix where
 
+import Control.Lens
 import Data.ByteString
-import Network.Wreq
 import qualified Data.Text as Text
+import Network.Wreq
 
 foo :: String
 foo = "bar"
+
+data ClientHandle = ClientHandle
+                  { username :: String
+                  , token    :: String
+                  } deriving (Show)
+
+login :: String -> String -> String -> IO ClientHandle
+login username' password homeserver =
+        pure ClientHandle
+            { username = username'
+            , token = "memes"
+            }
--- morpheus/test/Spec.hs
+++ morpheus/test/Spec.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.Matrix
+import Test.Hspec (Spec, hspec, describe, it, shouldSatisfy, shouldNotSatisfy, shouldBe)
+import System.Environment (getEnv)
+import qualified Data.Text as Text
+
+spec :: Spec
+spec =
+        describe "Matrix client" $ do
+            it "should let you log into an account" $ do
+                username' <- getEnv "MATRIX_USER"
+                password' <- getEnv "MATRIX_PASSWORD"
+                homeserver' <- getEnv "MATRIX_HOMESERVER"
+                c <- login username' password' homeserver'
+
+                shouldBe (username c) username'
+            it "has funciton foo" $ foo `shouldBe` "bar"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
